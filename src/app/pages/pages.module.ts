ba095000911945c6a5c390799813e7ee93283211
Eduardo <eduardowon@gmail.com>
2019-10-14 23:25:56 -0500

    ordenando estructura

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component
+  ],
+  imports: [
+    SharedModule,
+    PAGES_ROUTES
+  ]
+})
+
+export class PagesModule {}
